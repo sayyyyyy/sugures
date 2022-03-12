e2866369ea08a6c9340b05814b504fd6639eeb4d
青木聖弥 <aokikiyoshiwataru@aokikiyoshiwatarunoMacBook-Pro.local>
2022-03-12 22:28:01 +0900

    top->selectbox, jsファイル追加

--- application/application.py
+++ application/application.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def top():
-    return 'Hello world'
+    return render_template('top.html')
+
+@app.route('/list')
+def storelist():
+    return render_template('list.html')
+
+@app.route('/detail')
+def storedetail():
+    return render_template('detail.html')
---
+++ application/static/getdata.js
@@ -0,0 +1,8 @@
+
+
+function getstoredata (condition) {
+    const url_first = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
+    const api_key = '?key=' + '';
+    const api_url = url_first + api_key + condition;
+    
+}
