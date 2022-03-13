270e6ce5cc96257e5bc53b84d337268c7c854656
青木聖弥 <aokikiyoshiwataru@aokikiyoshiwatarunoMacBook-Pro.local>
2022-03-13 17:07:20 +0900

    API連携追加

--- application/application.py
+++ application/application.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+import requests
+import json
 
 app = Flask(__name__)
+load_dotenv()
 
 @app.route('/')
 def top():
+    if request.method == "POST":
+        getstoredata(10, 10 ,300)
     return render_template('top.html')
 
 @app.route('/list')
@@ -13,3 +20,34 @@ def storelist():
 @app.route('/detail')
 def storedetail():
     return render_template('detail.html')
+
+
+
+def getstoredata(usr_lat, usr_lng, search_range):
+
+    # API必要データ準備
+    hotpepper_api_url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+    hotpepper_api_key = os.getenv('HOTPEPPER_API_KEY')
+
+    search_start = 1
+    max_ammount = 100
+
+    query = {
+        'key': hotpepper_api_key,
+        'order': 1,
+        'start': search_start,
+        'count': max_ammount,
+        'lat': usr_lat,
+        'lng': usr_lng,
+        'range': search_range,
+        'format': 'json'
+    }
+
+    store_raw_data = requests.get(hotpepper_api_url, query)
+    store_data = json.loads(store_raw_data.text)['result']['shop']
+    
+    if len(store_data) == 0:
+        print("データがありません")
+        return 0
+    
+    print(store_data)
--- application/static/getdata.js
+++
@@ -1,8 +0,0 @@
-
-
-function getstoredata (condition) {
-    const url_first = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
-    const api_key = '?key=' + '';
-    const api_url = url_first + api_key + condition;
-    
-}
