5e1ccfe4b710b710bd34ee2721e4f66afe899578
青木聖弥 <aokikiyoshiwataru@aokikiyoshiwatarunoMacBook-Pro.local>
2022-03-15 22:17:59 +0900

    jsからflaskのapi呼び出す

--- application/application.py
+++ application/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from dotenv import load_dotenv
 import os
 import requests
@@ -10,19 +10,52 @@ load_dotenv()
 @app.route('/', methods=["GET", "POST"])
 def top():
     if request.method == "POST":
+        if request.form.get("random-search") != None:
+            search_method = "random"
+        else:
+            search_method = "search"
+            search_range = request.form.get('search-distance')
+        
+        getstoredata(10, 10 ,300)
         return redirect("/list")
-    return render_template('top.html')
+    else:
+        return render_template('top.html')
 
-@app.route('/list')
+@app.route('/list', methods=["GET", "POST"])
 def storelist():
     return render_template('list.html')
 
-@app.route('/detail')
+@app.route('/detail', methods=["GET", "POST"])
 def storedetail():
     return render_template('detail.html')
+    
+@app.route('/get_store_data/<lat>/<lng>', methods=["GET"])
+def aaa(lat, lng):
+    # API必要データ準備
+    hotpepper_api_url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/"
+    hotpepper_api_key = os.getenv('HOTPEPPER_API_KEY')
 
-def clickSearchBtn():
-    getstoredata(10, 10 ,300)
+    search_start = 1
+    max_ammount = 100
+
+    query = {
+        'key': hotpepper_api_key,
+        'order': 1,
+        'start': search_start,
+        'count': max_ammount,
+        # 'lat': usr_lat,
+        # 'lng': usr_lng,
+        # 'range': search_range,
+        'format': 'json'
+    }
+
+    store_raw_data = requests.get(hotpepper_api_url, query)
+    # store_data = json.loads(store_raw_data.text)['result']['shop']
+    
+    # if len(store_data) == 0:
+    #     print("データがありません")
+    #     return 0
+    return jsonify({'lat': lat, 'lng': lng})
 
 def getstoredata(usr_lat, usr_lng, search_range):
 
@@ -51,4 +84,5 @@ def getstoredata(usr_lat, usr_lng, search_range):
         print("データがありません")
         return 0
     
+    # json形式でstoreデータを返す
     print(store_data)
--- application/static/getdata.js
+++ application/static/getdata.js
@@ -5,10 +5,22 @@ function callGeolocation() {
 function successCallback(position) {
     const usr_lat = position.coords.latitude;
     const usr_lng = position.coords.longitude;
-    alert("緯度: " + usr_lat + "\n" + 
-    "経度: " + usr_lng);
+    accessapi(usr_lat, usr_lng);
 }
 
 function errorCallback(error) {
     alert(error);
+}
+
+function accessapi(lat, lng) {
+    const url = 'http://127.0.0.1:5000/get_store_data/' + lat + '/' + lng;
+    fetch(url)
+    .then((response) => {
+        if(!response.ok) {
+            console.log("データの取得に失敗しました。")
+        }
+        return response.json()
+    })
+    .then((json) => console.log(json))
+    .catch((error) => console.log(error))
 }
